0b8e76a3fef0be0e34daefc2e53829e1fd6dd607
Junior Santos <jnrplay@hotmail.com>
2022-02-23 23:13:32 -0300

    feat: project created

---
+++ src/styles/color-variables.ts
@@ -0,0 +1,9 @@
+export const variables = {
+  background: "#f0f2f5",
+  red: "#e52e4d",
+  blue: "#5229c",
+  blueLight: "#6933ff",
+  textTitle: "#363f5f",
+  textBody: "969cb3",
+  shape: "#ffffff",
+};
